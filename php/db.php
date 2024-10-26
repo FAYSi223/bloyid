fcd68b21b9f420b796255b0ddbb71ebf8936b8c5
FAYSi223 <134807221+FAYSi223@users.noreply.github.com>
2024-10-26 20:55:43 +0200

    Update db.php

--- php/db.php
+++ php/db.php
@@ -4,7 +4,7 @@ $host = "mysq-bloyid-bloyid.j.aivencloud.com";
 $port = "13978";
 $dbname = "defaultdb";
 $user = "avnadmin";
-$pass = "";
+$pass = "AVNS_GW1_3kxNz6GRj3-NDVj";
 
 try {
     $con = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $user, $pass);
